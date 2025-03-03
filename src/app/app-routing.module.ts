c428b65c9094f060adcca7f5d0f0335775d6374f
abdallah_abdalnaser <abdallahalbhare2002@gmail.com>
2025-03-03 04:32:55 +0200

    file Architecture

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/components/home/home.component';
+import { ProductsComponent } from './pages/components/products/products.component';
+import { SingleProductComponent } from './pages/components/single-product/single-product.component';
+import { LoginComponent } from './pages/components/login/login.component';
+import { RegisterComponent } from './pages/components/register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:HomeComponent},
+  {path:'home',component:HomeComponent},
+  {path:'Products',component:ProductsComponent},
+  {path:'SingleProduct',component:SingleProductComponent},
+  {path:'Login',component:LoginComponent},
+  {path:'Register',component:RegisterComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/pages/pages.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HomeComponent } from './components/home/home.component';
+import { ProductsComponent } from './components/products/products.component';
+import { SingleProductComponent } from './components/single-product/single-product.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+
+
+
+@NgModule({
+  declarations: [
+    HomeComponent,
+    ProductsComponent,
+    SingleProductComponent,
+    LoginComponent,
+    RegisterComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    HomeComponent,
+    ProductsComponent,
+    SingleProductComponent,
+    LoginComponent,
+    RegisterComponent
+  ]
+})
+export class PagesModule { }
